bab862ff95643dd098d4dda80e2819fb9b952fa4
SCARRAIDER <rsahanediriweera@gmail.com>
2023-02-14 20:36:21 +0530

    code cleaning and implementation of image difference

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ from datetime import datetime
 import os
 
 
+def get_Video_Capture_and_Write(vsource,outputname):
+    cap = cv2.VideoCapture(vsource)
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+    fourcc = cv2.VideoWriter_fourcc('P','I','M','1')
+    outputname = getFileName() if outputname == "output" else outputname
+    video_writer = cv2.VideoWriter(os.path.join("Recordings","{}.avi".format(outputname)),fourcc,fps,(width,height))
+    return cap,video_writer
+
+
 def getFileName():
     now = datetime.now()
     dt_value = now.strftime("%d/%m/%Y %H:%M%S")
@@ -13,29 +24,19 @@ def getFileName():
 
 def run(vsource = 0,outputname = "output",threshold = 1000,timerlevel = 60):
     
-    cap = cv2.VideoCapture(vsource)
-    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    fps = int(cap.get(cv2.CAP_PROP_FPS))
-    fourcc = cv2.VideoWriter_fourcc('P','I','M','1')
-    outputname = getFileName() if outputname == "output" else outputname
-    video_writer = cv2.VideoWriter(os.path.join("Recordings","{}.avi".format(outputname)),fourcc,fps,(width,height))
-
+    cap,video_writer = get_Video_Capture_and_Write(vsource= vsource,outputname=outputname)
     while cap.isOpened():
         try:
             ret, frame = cap.read()
             if(True):
                 video_writer.write(frame)
-            if((True)OR(True)):
+            if(True):
                 print("Send Email")
 
             if cv2.waitKey(10) & 0xFF == ord('q'):
                 break
         except e:
             break
-
-
-
 
 def parse_opt():
     parser = argparse.ArgumentParser()
