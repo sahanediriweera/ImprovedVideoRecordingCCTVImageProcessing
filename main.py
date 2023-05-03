89172fa6a4a3a72b3945698cc6090eb73707290b
SCARRAIDER <rsahanediriweera@gmail.com>
2023-05-03 15:59:04 +0530

    fixed unintended bugs

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ from SafetyDetection import fireDetection,smokeDetection
 
 def get_Video_Capture_and_Write(outputname):
     outputname = getFileName() if outputname == "output" else outputname
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')  # Choose the codec (XVID for .avi format)
-    video_writer = cv2.VideoWriter("output.avi", fourcc, 20.0, (640, 480))  # Create the VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc(*'XVID') 
+    video_writer = cv2.VideoWriter("output.avi", fourcc, 20.0, (640, 480))
     cap = cv2.VideoCapture(0)
     return cap,video_writer
 
@@ -50,11 +50,13 @@ def process_video(algorithm,outputname,threshold,timerlevel,reference_image,vide
         print()
 
 def run(vsource = 0,outputname = "output",threshold = 1000,timerlevel = 60,reference_image = "",video_link="",algorithm="pixel"):
+    threshold = float(threshold[0])
+    timerlevel = int(timerlevel[0])
     if(video_link == "" ):
         if(algorithm == "historgram"):
-            IDif = ImageHistogramDifference(reference_image,threshold=float(threshold[0]),timerlevel=timerlevel)
+            IDif = ImageHistogramDifference(reference_image,threshold=threshold,timerlevel=timerlevel)
         else:
-            IDif = ImagePixelDifference(reference_image,threshold=float(threshold[0]),timerlevel=timerlevel)
+            IDif = ImagePixelDifference(reference_image,threshold=threshold,timerlevel=timerlevel)
         fourcc = cv2.VideoWriter_fourcc(*'XVID')  # Choose the codec (XVID for .avi format)
         video_writer = cv2.VideoWriter("{}.avi".format(outputname), fourcc, 20.0, (640, 480))  # Create the VideoWriter object
         cap = cv2.VideoCapture(vsource)
@@ -83,7 +85,7 @@ def parse_opt():
     parser = argparse.ArgumentParser()
     parser.add_argument("--vsource",nargs= '+',type= int,default=0,help= "Enter the Video camara value")
     parser.add_argument("--outputname",default= "output",help= "Enter the name of the file name ")
-    parser.add_argument("--threshold",nargs= '+',type= float,default=1000,help= "Enter the threshold value")
+    parser.add_argument("--threshold",nargs= '+',type= float,default=1000.0,help= "Enter the threshold value")
     parser.add_argument("--timerlevel",nargs= '+',type=int,default=60,help= "Enter the timer level to compare")
     parser.add_argument("--reference_image",default="",help="Enter the address of the reference image")
     parser.add_argument("--video_link",default="",help="Enter Video Addrress")
